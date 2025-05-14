18eee656ddc84a66732e6f3b0bd49f34c119e464
Luis Donis <ldonis.emc2@gmail.com>
2025-05-13 20:32:31 -0600

    fix password reset responses

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -4,42 +4,44 @@ class Users::PasswordsController < Devise::PasswordsController
 
     # Sends an email with a token, so the user can reset their password
     def create
-
-        if params[:user].blank?
-            #Account::Activity.log("core", "/password/create", "password_creation_failed", "no_valid_email")
-            return respond_with_error(I18n.t("core.shared.messages_warning_user_not_found"))
-        end
-
-        if params[:user][:email].blank?
-            #Account::Activity.log("core", "/password/create", "password_creation_failed", "no_valid_email")
-            return respond_with_error(I18n.t("core.shared.messages_warning_user_not_found"))
-        end
-
-        user = Lesli::User.find_by(:email => params[:user][:email])
-
-        if user.blank?
-            # Account::Activity.log("core", "/password/create", "password_creation_failed", "no_valid_email", {
-            #     email: (params[:user][:email] || "")
-            # })
-            return respond_with_error(I18n.t("core.shared.messages_warning_user_not_found"))
-        end
-
-        unless user.active
-            # user.logs.create({title: "password_creation_failed", description: "user_not_active"})
-            # Account::Activity.log("core", "/password/create", "password_creation_failed", "user_not_active")
-            return respond_with_error(I18n.t("core.users/passwords.messages_danger_inactive_user"))
-        end
-
-        token = user.generate_password_reset_token
-
-        #user.logs.create({ title: "password_creation_successful" })
-
         begin
+
+            if params[:user].blank?
+                #Account::Activity.log("core", "/password/create", "password_creation_failed", "no_valid_email")
+                raise(I18n.t("core.shared.messages_warning_user_not_found"))
+            end
+
+            if params[:user][:email].blank?
+                #Account::Activity.log("core", "/password/create", "password_creation_failed", "no_valid_email")
+                raise(I18n.t("core.shared.messages_warning_user_not_found"))
+            end
+
+            user = Lesli::User.find_by(:email => params[:user][:email])
+
+            if user.blank?
+                # Account::Activity.log("core", "/password/create", "password_creation_failed", "no_valid_email", {
+                #     email: (params[:user][:email] || "")
+                # })
+                raise(I18n.t("core.shared.messages_warning_user_not_found"))
+            end
+
+            unless user.active
+                user.activities.create({title: "password_creation_failed", description: "user_not_active"})
+                # Account::Activity.log("core", "/password/create", "password_creation_failed", "user_not_active")
+                raise(I18n.t("core.users/passwords.messages_danger_inactive_user"))
+            end
+
+            token = user.generate_password_reset_token
+
+            user.activities.create({ title: "password_create", description: "Password reset instructions sent" })
+
             Lesli::DeviseMailer.reset_password_instructions(user, token).deliver_now
-            respond_with_successful
+            success(I18n.t("core.users/passwords.messages_success"))
+            redirect_to(new_user_password_path)
         rescue => exception
             #Honeybadger.notify(exception)
-            respond_with_error(exception.message)
+            danger(exception.message)
+            redirect_to(new_user_password_path)
         end
     end
 

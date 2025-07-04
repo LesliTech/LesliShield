07ebc8455e733063dec1d21965f54a6d2faf70ef
Luis Donis <ldonis.emc2@gmail.com>
2025-07-03 21:41:34 -0600

    update docs

--- app/controllers/lesli_shield/role/actions_controller.rb
+++ app/controllers/lesli_shield/role/actions_controller.rb
@@ -3,11 +3,20 @@ module LesliShield
         #before_action :set_dashboard_component, only: %i[ show edit update destroy ]
 
         def update
+            pp "---   ---   ---   ---   ---   ---   ---   ---"
+            pp params[:role_id]
+            pp "---   ---   ---   ---   ---   ---   ---   ---"
             success("Role privileges updated successfully!")
             respond_to do |format|
                 format.turbo_stream
                 format.html { redirect_to @role }
             end
+        end
+
+        def role_action_params
+            params.require(:role_action).permit(
+                :role_id
+            )
         end
     end
 end

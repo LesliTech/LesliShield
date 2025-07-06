8db116f65caeeb2b43b6b25d22f007617da42fdc
Luis Donis <ldonis.emc2@gmail.com>
2025-07-05 21:00:05 -0600

    add simple role action edition

--- app/controllers/lesli_shield/role/actions_controller.rb
+++ app/controllers/lesli_shield/role/actions_controller.rb
@@ -1,16 +1,39 @@
 module LesliShield
     class Role::ActionsController < ApplicationController
-        #before_action :set_dashboard_component, only: %i[ show edit update destroy ]
+        before_action :set_role_action, only: %i[update destroy ]
 
         def update
-            pp "---   ---   ---   ---   ---   ---   ---   ---"
-            pp params[:role_id]
-            pp "---   ---   ---   ---   ---   ---   ---   ---"
-            success("Role privileges updated successfully!")
-            respond_to do |format|
-                format.turbo_stream
-                format.html { redirect_to @role }
+            # check saved
+            if @role_action.result.recover
+                success("Role privileges added successfully!")
+                respond_to do |format|
+                    format.turbo_stream
+                    render turbo_stream: turbo_stream.replace("application-lesli-notifications", partial: "lesli/partials/application-lesli-notifications")
+                    #format.html { redirect_to role_path(@role_action.role_id) }
+                end
+            else 
+                respond_with_error(@user.errors)
             end
+        end
+
+        def destroy
+            # check saved
+            if @role_action.result.destroy
+                success("Role privileges removed successfully!")
+                respond_to do |format|
+                    format.turbo_stream
+                    #format.html { redirect_to role_path(@role_action.role_id) }
+                end
+            else 
+                respond_with_error(@user.errors)
+            end
+        end
+
+        private
+
+        # Use callbacks to share common setup or constraints between actions.
+        def set_role_action
+            @role_action = Lesli::Role::ActionService.new(current_user).find(params[:id])
         end
 
         def role_action_params

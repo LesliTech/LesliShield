ee93c70cc2a1c68bbb7d822d8f4c800918597476
Luis Donis <ldonis.emc2@gmail.com>
2026-01-21 21:25:54 -0600

    Fix user update

--- app/controllers/lesli_shield/users_controller.rb
+++ app/controllers/lesli_shield/users_controller.rb
@@ -41,12 +41,13 @@ module LesliShield
 
         # GET /users/1
         def show
-            @activities = @user.result.activities.order(id: :desc).map { |a| {
-                id: a[:id],
-                title: a[:title].titleize,
-                description: a[:description],
-                date: Date2.new(a[:created_at]).date_words
-            }}
+            # @activities = @user.result.activities.order(id: :desc).map { |a| {
+            #     id: a[:id],
+            #     title: a[:title].titleize,
+            #     description: a[:description],
+            #     date: Date2.new(a[:created_at]).date_words
+            # }}
+            @activities = []
             @sessions = @user.result.sessions
             @user = @user.show
         end
@@ -78,35 +79,36 @@ module LesliShield
         # PATCH/PUT /users/1
         def update
 
-            # check if the user trully exists
-            return respond_with_not_found unless @user.found?
-
             # update the user information
             @user.update(user_params)
 
             # check saved
             if @user.successful?
-                success("User updated successfully!")
-                respond_to do |format|
-                    format.turbo_stream
-                    format.html { redirect_to @user }
-                end
+                respond_with_lesli(
+                    :turbo => stream_notification_success("User updated successfully!")
+                )
             else 
-                respond_with_error(@user.errors)
+                respond_with_lesli(
+                    :turbo => stream_notification_danger(@user.errors_as_sentence)
+                )
             end
         end
 
         # DELETE /users/1
         def destroy
             @user.destroy!
-            redirect_to users_path, notice: "User was successfully destroyed.", status: :see_other
+            redirect_to(users_path, notice: "User was successfully destroyed.", status: :see_other)
         end
 
         private
 
-        # Use callbacks to share common setup or constraints between actions.
         def set_user
+
+            # Search for the user
             @user = Lesli::UserService.new(current_user).find(params[:id])
+
+            # check if the user trully exists
+            return respond_with_not_found unless @user.found?
         end
 
         # Only allow a list of trusted parameters through.
